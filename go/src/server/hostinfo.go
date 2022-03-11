ddf40bee4d193dd4505050ca6d87450aaecc6cad
Stridsvagn69420 <Stridsvagn69420@users.noreply.github.com>
2022-03-11 01:47:53 +0100

    Hostinfo with Albums and size finally done

--- go/src/server/hostinfo.go
+++ go/src/server/hostinfo.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"encoding/json"
+	"io/ioutil"
+	"path"
 	"strconv"
 
 	"Cyrkensia/utils"
@@ -20,21 +22,23 @@ type Hostinfo struct {
 	Albums    []Album `json:"albums"`
 }
 type Album struct {
-	Cover string `json:"cover"`
-	Dir   string `json:"dir"`
-	Name  string `json:"name"`
-	Files []File `json:"files"`
+	Cover string      `json:"cover"`
+	Dir   string      `json:"dir"`
+	Name  string      `json:"name"`
+	Files []MusicFile `json:"files"`
 }
-type File struct {
+type MusicFile struct {
 	Name string `json:"name"`
 	Size int    `json:"size"`
 }
 
+type metadata struct {
+	Name  string `json:"name"`
+	Cover string `json:"cover"`
+}
+
 func HostinfoEndpoint(c *fiber.Ctx) error {
 	// Generate hostinfo
-	size, err := utils.DirSize(utils.Config.CDNpath)
-	ServerError500(c, err)
-	// TODO: add albums
 	hostinfo := Hostinfo{
 		Name:      utils.Config.Name,
 		Hosticon:  utils.Config.Icon,
@@ -42,12 +46,72 @@ func HostinfoEndpoint(c *fiber.Ctx) error {
 		Secured:   utils.Config.Locked,
 		Root:      "",
 		OriginURI: c.Protocol() + "://" + c.Hostname() + c.Path(),
-		Size:      int(size),
 	}
+	hostinfo.Albums, hostinfo.Size = readAlbums(c, utils.Config.CDNpath)
 	// send hostinfo response
 	json, err := json.Marshal(hostinfo)
 	ServerError500(c, err)
 	c.Append("Content-Type", fiber.MIMEApplicationJSON)
 	c.Append("Content-Length", strconv.Itoa(len(json)))
 	return c.Status(fiber.StatusOK).Send(json)
+}
+
+// Hostinfo specific IO functions for reading out available songs and albums
+func readAlbums(c *fiber.Ctx, pathdir string) ([]Album, int) {
+	size := 0
+	files, err := ioutil.ReadDir(pathdir)
+	albums := make([]Album, len(files))
+	if err != nil {
+		ServerError500(c, err)
+	}
+	for i, file := range files {
+		if file.IsDir() {
+			albumpath := path.Join(pathdir, file.Name())
+			meta := readMetadata(c, albumpath)
+			album := Album{
+				Name:  meta.Name,
+				Dir:   file.Name(),
+				Cover: meta.Cover,
+				Files: readFiles(c, albumpath, &size),
+			}
+			albums[i] = album
+		}
+	}
+	if len(albums) == 0 {
+		albums = make([]Album, 0)
+	}
+	return albums, size
+}
+
+func readMetadata(c *fiber.Ctx, dirpath string) metadata {
+	var metadata metadata
+	file, err := ioutil.ReadFile(path.Join(dirpath, ".metadata.json"))
+	if err != nil {
+		ServerError500(c, err)
+	}
+	err = json.Unmarshal(file, &metadata)
+	if err != nil {
+		ServerError500(c, err)
+	}
+	return metadata
+}
+
+func readFiles(c *fiber.Ctx, dirpath string, size *int) []MusicFile {
+	files, err := ioutil.ReadDir(dirpath)
+	musicfiles := make([]MusicFile, len(files))
+	if err != nil {
+		ServerError500(c, err)
+	}
+	for i, file := range files {
+		if !file.IsDir() {
+			filesize := int(file.Size())
+			file := MusicFile{
+				Name: file.Name(),
+				Size: filesize,
+			}
+			*size += filesize
+			musicfiles[i] = file
+		}
+	}
+	return musicfiles
 }
