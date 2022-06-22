fcf459da690abc5c5ae44fc879f65004ca2400b2
Stridsvagn69420 <Stridsvagn69420@users.noreply.github.com>
2022-06-22 20:22:30 +0200

    Fix filesystem reading issues I discovered and just list filenames

--- php/hostinfo.php
+++ php/hostinfo.php
@@ -1,5 +1,5 @@
 <?php 
-$config = json_decode(file_get_contents("/etc/discordrpc-server/config.json"));
+$config = json_decode(file_get_contents("/etc/cyrkensia/config.json"));
 $albums = glob($config->root."/*");
 function getOriginURI() {
     $uri_paths = explode('?', $_SERVER['REQUEST_URI'], 2);
@@ -16,31 +16,30 @@ $output = array(
     "size" => 0
 );
 foreach ($albums as $album) {
-    $folderName = str_replace($config->root."/", "", $album);
-    $absoluteDir = $config->root."/".$folderName;
-    $metadata = json_decode(file_get_contents($absoluteDir."/.metadata.json"));
-    $allFiles = glob($absoluteDir."/*");
-    $musicFiles = array();
-    foreach ($allFiles as $path) {
-        $audioSize = filesize($path);
-        $output["size"] += $audioSize;
-        array_push(
-            $musicFiles,
-            $fileEntry = array(
-                "name" => str_replace($absoluteDir."/", "", $path),
-                "size" => $audioSize
-            )
+    if (is_dir($album)) {
+        $folderName = str_replace($config->root."/", "", $album);
+        $absoluteDir = $config->root."/".$folderName;
+        $metadata = json_decode(file_get_contents($absoluteDir."/.metadata.json"));
+        $allFiles = glob($absoluteDir."/*");
+        $musicFiles = array();
+        foreach ($allFiles as $path) {
+            $audioSize = filesize($path);
+            $output["size"] += $audioSize;
+            array_push(
+                $musicFiles,
+                str_replace($absoluteDir."/", "", $path)
+            );
+        }
+        $albumObject = array(
+            "dir" => $folderName,
+            "name" => $metadata->name,
+            "cover" => $metadata->cover,
+            "files" => $musicFiles
         );
+        array_push($output['albums'], $albumObject);
     }
-    $albumObject = array(
-        "dir" => $folderName,
-        "name" => $metadata->name,
-        "cover" => $metadata->cover,
-        "files" => $musicFiles
-    );
-    array_push($output['albums'], $albumObject);
 }
-$result = json_encode($output, JSON_PRETTY_PRINT);
+$result = json_encode($output);
 header("Content-Type: application/json");
 header("Content-Length: ".strlen($result));
 echo $result;
