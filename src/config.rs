63fb5a0e6b574b2b00b82326bba24d09f3ab7eb2
Stridsvagn69420 <Stridsvagn69420@users.noreply.github.com>
2024-01-22 00:51:33 +0100

    Optimized db types Config and User a bit

--- src/config.rs
+++ src/config.rs
@@ -1,14 +1,18 @@
-use std::fmt;
 use std::convert::TryFrom;
 use serde::{Serialize, Deserialize};
 use serde_json;
 
-#[derive(Serialize, Deserialize, Hash, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Serialize, Deserialize, PartialEq, Eq)]
 pub struct Config {
 	/// Server Name
 	/// 
 	/// The Name of the Cyrkensia server.
 	pub name: String,
+
+	/// Contact
+	/// 
+	/// A URL or E-Mail address to contact the server's maintainers.
+	pub contact: String,
 
 	/// Bind Address
 	/// 
@@ -16,11 +20,16 @@ pub struct Config {
 	/// On Unix platforms, this can also be a path to a Unix Domain Socket.
 	pub addr: String,
 
-	/// Database Path
+	/// Music Database Path
 	/// 
-	/// Path to the folder that contains the
-	/// `artists.json`, `albums.json`, `tracks.json`, `users.json` and `maintainers.json` database files.
-	pub database: String
+	/// Path to a folder that contains the `artists.json`, `album.json` and `tracks.json`.
+	pub database: String,
+
+	/// User Database Path
+	/// 
+	/// Path to a `users.json` file. If this is not set, the server will not ask for authorization.
+	/// If it is set, but the file fails to be read, the server will not start.
+	pub accounts: Option<String>
 }
 
 impl TryFrom<&str> for Config {
@@ -44,11 +53,5 @@ impl TryFrom<&[u8]> for Config {
 
 	fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
 		serde_json::from_slice(value)
-	}
-}
-
-impl fmt::Display for Config {
-	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-		write!(f, "{}@{} ({})", self.name, self.addr, self.database)
 	}
 }
